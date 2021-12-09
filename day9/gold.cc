6856087fddaf3e670e5374eae55a8109bda3f21f
feydor <vreyes85739@outlook.com>
2021-12-09 14:12:33 -0700

    Clean up day9 some more.

--- day9/gold.cc
+++ day9/gold.cc
@@ -3,79 +3,71 @@
 #include <vector>
 using namespace std;
 
-struct Point {
-    int row, col;
-    int n;
-};
+typedef pair<int, int> Point;
 
 // bounds-checking
-Point get_neighbor(const vector<vector<Point>> &mat, int r, int c) {
+int get_neighbor(const vector<string> &mat, int row, int col) {
     auto last_r = mat.size()-1;
     auto last_c = mat[0].size()-1;
 
-    if (r < 0 || r > last_r || c < 0 || c > last_c)
-        return {.row = -1, .col = -1, .n = INT32_MAX};
-    return mat[r][c];
+    if (row < 0 || row > last_r || col < 0 || col > last_c)
+        return INT32_MAX;
+    return mat[row][col]-'0';
 }
 
 // returns true if the vector v holds a point with the same coordinates as p
-bool point_in_vec(const vector<Point> &v, const Point &p) {
-    return any_of(begin(v), end(v), [&](Point other){ return other.row == p.row && other.col == p.col;});
+bool point_in_vec(const vector<Point> &v, int row, int col) {
+    return any_of(begin(v), end(v), [&](Point other){ return other.first == row && other.second == col;});
 }
 
-void add_neighbors(vector<Point> &basin, const vector<vector<Point>> &h, int row, int col) {
-    basin.push_back(h[row][col]);
+void add_neighbors(vector<Point> &basin, const vector<string> &mat, int row, int col) {
+    basin.push_back({row, col});
 
-    auto above = get_neighbor(h, row-1, col);
-    auto below = get_neighbor(h, row+1, col);
-    auto left = get_neighbor(h, row, col-1);
-    auto right = get_neighbor(h, row, col+1);
+    auto above = get_neighbor(mat, row-1, col);
+    auto below = get_neighbor(mat, row+1, col);
+    auto left = get_neighbor(mat, row, col-1);
+    auto right = get_neighbor(mat, row, col+1);
 
-    if (above.n < 9 && !point_in_vec(basin, above)) {
-        add_neighbors(basin, h, row-1, col);
+    if (above < 9 && !point_in_vec(basin, row-1, col)) {
+        add_neighbors(basin, mat, row-1, col);
     }
-    if (below.n < 9 && !point_in_vec(basin, below)) {
-        add_neighbors(basin, h, row+1, col);
+    if (below < 9 && !point_in_vec(basin, row+1, col)) {
+        add_neighbors(basin, mat, row+1, col);
     }
-    if (left.n < 9 && !point_in_vec(basin, left)) {
-        add_neighbors(basin, h, row, col-1);
+    if (left < 9 && !point_in_vec(basin, row, col-1)) {
+        add_neighbors(basin, mat, row, col-1);
     }
-    if (right.n < 9 && !point_in_vec(basin, right)) {
-        add_neighbors(basin, h, row, col+1);
+    if (right < 9 && !point_in_vec(basin, row, col+1)) {
+        add_neighbors(basin, mat, row, col+1);
     }
 }
 
 // 736920
 int main() {
     string str;
-    vector<vector<Point>> h;
-    int r = 0, c = 0;
+    vector<string> mat;
     while (getline(cin, str)) {
-        vector<Point> line;
-        for (const auto &n : str) {
-            line.push_back({.row = r, .col = c++, .n = n-'0'});
-        }
-        h.push_back(line);
-        r++;
+        mat.push_back(str);
     }
     
-    vector<vector<Point>> basins;
+    vector<uint64_t> basin_sizes;
     vector<pair<int, int>> d = {{-1, 0}, {0, 1}, {1, 0}, {0, -1}};
-    for (int row = 0; row < h.size(); ++row) {
-        for (int col = 0; col < h[row].size(); ++col) {
-            Point p = h[row][col];
+    for (int row = 0; row < mat.size(); ++row) {
+        for (int col = 0; col < mat[row].size(); ++col) {
+            int p = mat[row][col]-'0';
 
             // local min
-            if (all_of(begin(d), end(d), [&](auto d){return get_neighbor(h, row+d.first, col+d.second).n > p.n;})){
+            if (all_of(begin(d), end(d), [&](auto d){return get_neighbor(mat, row+d.first, col+d.second) > p;})){
                 vector<Point> basin;
-                add_neighbors(basin, h, row, col);
-                basins.push_back(basin);
+                add_neighbors(basin, mat, row, col);
+                basin_sizes.push_back(basin.size());
             }
         }
     }
 
-    sort(begin(basins), end(basins), [](auto a, auto b){return a.size() > b.size();});
-    cout << "three largest basin sizes: " << basins.at(0).size() << " " << basins.at(1).size() << " " << basins.at(2).size() << "\n";
-    cout << "product: " << basins.at(0).size()*basins.at(1).size()*basins.at(2).size() << "\n";
+    sort(begin(basin_sizes), end(basin_sizes), [](auto a, auto b){return a > b;});
+
+    cout << "three largest basin sizes: " << basin_sizes.at(0) << " " << basin_sizes.at(1) << " " << basin_sizes.at(2) << "\n";
+    cout << "product: " << basin_sizes.at(0)*basin_sizes.at(1)*basin_sizes.at(2) << "\n";
     return 0;
 }
--- day9/silver.cc
+++ day9/silver.cc
@@ -6,12 +6,12 @@ using namespace std;
 
 vector<pair<int, int>> d = {{-1, 0}, {0, 1}, {1, 0}, {0, -1}};
 
-vector<int> get_neighbors(const vector<vector<int>> &mat, int row, int col) {
+vector<int> get_neighbors(const vector<string> &mat, int row, int col) {
     vector<int> neighbors;
     for (const auto &[dy, dx] : d) {
         int y = row+dy, x = col+dx;
         if (x >= 0 && x < mat[0].size() && y >= 0 && y < mat.size())
-            neighbors.push_back(mat[y][x]);
+            neighbors.push_back(mat[y][x]-'0');
     }
     return neighbors;
 }
@@ -19,23 +19,20 @@ vector<int> get_neighbors(const vector<vector<int>> &mat, int row, int col) {
 // 539
 int main() {
     string str;
-    vector<vector<int>> mat;
+    vector<string> mat;
     while (getline(cin, str)) {
-        vector<int> line;
-        transform(begin(str), end(str), back_inserter(line), [](char c){return c-'0';});
-        mat.push_back(line);
+        mat.push_back(str);
     }
     
     vector<int> sinks;
     for (int row = 0; row < mat.size(); ++row) {
         for (int col = 0; col < mat[row].size(); ++col) {
-            auto x = mat[row][col];
+            auto x = mat[row][col]-'0';
             auto neighbors = get_neighbors(mat, row, col);
             if (all_of(begin(neighbors), end(neighbors), [&](auto n){return n > x;}))
                 sinks.push_back(x);
         }
     }
-
     cout << reduce(begin(sinks), end(sinks), 0, [](auto a, auto b){return a+b+1;}) << endl;
     return 0;
 }
