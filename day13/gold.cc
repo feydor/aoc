2396e32d68fea24d117717f9e47fbcd636f9f9d0
feydor <vreyes85739@outlook.com>
2021-12-13 11:41:31 -0700

    Clean up day13 some more.

--- day13/gold.cc
+++ day13/gold.cc
@@ -4,6 +4,7 @@
 #include <iterator>
 #include <sstream>
 #include <vector>
+#include "bit_matrix.h"
 using namespace std;
 #define SIZE 1312 // the maximum value of the input + padding
 
@@ -18,73 +19,11 @@ vector<string> split(const string &s, char delimiter = ' ') {
 	return splits;
 }
 
-bool get(bool *mat, int row, int col) {
-	return mat[row + col*SIZE];
-}
-
-void set(bool *mat, int row, int col) {
-	mat[row + col*SIZE] = true;
-}
-
-void clear(bool *mat, int row, int col) {
-	mat[row + col*SIZE] = false;
-}
-
-void fold_x(bool *mat, int n) {
-	// start at fold line
-	// shift left
-	for (size_t row = 0; row < SIZE; ++row) {
-		int col_count = 0;
-		for (size_t col = n; col < SIZE; ++col) {
-			bool is_set = get(mat, row, col);
-			if (is_set) {
-				clear(mat, row, col);
-				set(mat, row , col-col_count);
-			}
-			col_count +=2;
-		}
-	}
-}
-
-void fold_y(bool *mat, int n) {
-	// start at fold line
-	// shift up
-	int row_count = 0;
-	for (size_t row = n; row < SIZE; ++row) {
-		for (size_t col = 0; col < SIZE; ++col) {
-			if (get(mat, row, col)) {
-				clear(mat, row, col);
-				set(mat, row-row_count, col);
-			}
-		}
-		row_count += 2;
-	}
-}
-
-void print_mat(bool *mat) {
-	for (size_t row = 0; row < 10; ++row) {
-		for (size_t col = 0; col < 50; ++col) {
-			cout << (get(mat, row, col) ? "X" : ".");
-		}
-		cout << "\n";
-	}
-}
-
-int n_visible(bool *mat) {
-	int count = 0;
-	for (size_t r = 0; r < SIZE; ++r) {
-		for (size_t c = 0; c < SIZE; ++c) {
-			count += get(mat, r, c) ? 1 : 0;
-		}
-	}
-	return count;
-}
-
 // silver: 716
 // gold: RPCKFBLR
 int main() {
 	string line;
-	bool *mat = new bool[SIZE*SIZE];
+	bit_matrix mat(SIZE);
 	vector<string> instructions;
 	while (getline(cin, line)) {
 		if (line.empty()) continue;
@@ -92,7 +31,7 @@ int main() {
 			instructions.push_back(line);
 		} else {
 			auto coord = split(line, ',');
-			set(mat, stoi(coord[1]), stoi(coord[0]));
+			mat.set(stoi(coord[1]), stoi(coord[0]));
 		}
 	}
 
@@ -100,17 +39,16 @@ int main() {
 		auto tokens = split(instr);
 		auto equals_sign = tokens[2].find('=');
 		int n = stoi(tokens[2].substr(equals_sign+1));
-		
+
 		if (tokens[2][0] == 'y') {
-			fold_y(mat, n);
+			mat.fold_y(n);
 		} else {
-			fold_x(mat, n);
+			mat.fold_x(n);
 		}
 	}
 
-	cout << "dots: " << n_visible(mat) << endl;
-	print_mat(mat);
+	cout << "dots: " << mat.n_set() << endl;
+	cout << mat << endl;
 
-	delete[] mat;
 	return 0;
 }
