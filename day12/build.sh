d60e423eb5af987dbc547d947719dd39cba772c7
feydor <vreyes85739@outlook.com>
2021-12-14 10:45:04 -0700

    Finish day12 silver.

--- day12/build.sh
+++ day12/build.sh
@@ -1,6 +1,7 @@
 #!/bin/sh
 CXX=c++
 CXXFLAGS="-Wall -Wextra -Wshadow -std=c++20 -pedantic"
+CXXINCLUDE="-I ../"
 set -xe
 
-$CXX $CXXFLAGS -o silver silver.cc && cat example | ./silver
+$CXX $CXXFLAGS $CXXINCLUDE -o silver silver.cc && cat example | ./silver
--- day12/silver.cc
+++ day12/silver.cc
@@ -1,4 +1,5 @@
 #include <algorithm>
+#include <cstring>
 #include <cctype>
 #include <iostream>
 #include <iterator>
@@ -6,101 +7,94 @@
 #include <sstream>
 #include <ranges>
 #include <vector>
+#include "aoc.hpp"
 using namespace std;
 
-vector<string_view> split(string_view s, char delimiter = ' ') {
-	vector<string_view> result;
-	auto split = s
-		| ranges::views::split(delimiter)
-		| ranges::views::transform([](auto&& str) { return string_view(&*str.begin(), ranges::distance(str)); });
-	for (const auto word : split)
-		result.push_back(word);
-	return result;
+// build an adjacency matrix
+#define GRAPH_CAP 15
+bool graph[GRAPH_CAP][GRAPH_CAP];
+vector<uint32_t> visited(GRAPH_CAP, 0);
+vector<string> vertices;
+
+bool is_large_cave(unsigned int idx) {
+	return isupper(vertices.at(idx)[0]);
 }
 
-void print_map(const map<string, int> &m) {
-    cout << "\nprinting map...\n";
-    for (const auto &[s, n] : m)
-        cout << s << " : " << n << endl;
+void print_graph() {
+	for (int y = 0; y < GRAPH_CAP; ++y) {
+		for (int x = 0; x < GRAPH_CAP; ++x) {
+			cout << (graph[y][x] ? "X" : ".") << " ";
+		}
+		cout << "\n";
+	}
+}
+// returns an index based on location in vertices
+uint64_t vertex_idx(const string &s) {
+	// if already is in graph, return corresponding idx
+	// otherwise add it to list of vertices
+	auto result = find(begin(vertices), end(vertices), s);
+	if (result != vertices.end()) {
+		return distance(begin(vertices), result);
+	} else {
+		vertices.push_back(s);
+		return vertices.size()-1;
+	}
+	return 0;
 }
 
-bool bfs(map<string, vector<string>> &graph, map<string, int> &visited, string node, uint64_t &paths) {
-    cout << node << " ";
-    if (node == "end")
-        return false;
+// start is visited, end is next
+void dfs(uint32_t start, uint32_t end, uint64_t &paths) {
+	// cout << vertices[start] << " ";
+	if (start == end) {
+		// cout << "\n";
+		paths++;
+		return;
+	}
 
-    visited[node]++;
-    print_map(visited);
-
-    paths++;
-    
-    auto neighbors = graph[node];
-    for (const auto &edge : neighbors) {
-        if (edge== "start")
-            continue;
-
-        // lower case and visited more than once, skip
-        if (any_of(begin(edge), end(edge), [](char c){return isupper(c) == 0;}) && visited[edge] > 0)
-            continue;
-
-        bfs(graph, visited, edge, paths);
-    }
-    return true;
+	// for all neighbors w of s in Graph g
+	for (size_t i = 0; i < vertices.size(); ++i) {
+		// if adjacent and not visited
+		if (graph[start][i] && !visited[i]) {
+			if (!is_large_cave(i)) visited[i] = true;
+			dfs(i, end, paths);
+			visited[i] = false; // after unroll, clean up trail
+		}
+	}
 }
-
 
 int main() {
     vector<string> lines(istream_iterator<string>(cin), istream_iterator<string>(), {});
     vector<pair<string, string>> edges;
     for (const auto &line : lines) {
-		auto tokens = split(line, '-');
+		auto tokens = aoc::split(line, '-');
         edges.emplace_back(tokens[0], tokens[1]);
     }
     
     int n_edges = edges.size();
     cout << "n_edges: " << n_edges << endl; 
 
-    map<string, vector<string>> graph;
+	memset(graph, false, sizeof graph);
     for (const auto &[start, end] : edges) {
-        auto adj_edges = graph.find(start);
-
-        // already exists, add 'end' to adj_edges.second
-        if (adj_edges != graph.end()) {
-            adj_edges->second.push_back(end);
-        } else {
-            vector<string> adjacent = {end};
-            graph[start] = adjacent;
-        }
-
-        // do the same for the end
-        auto end_adj_edges = graph.find(end);
-        if (end_adj_edges != graph.end()) {
-            end_adj_edges->second.push_back(start);
-        } else {
-            vector<string> adjacent = {start};
-            graph[end] = adjacent;
-        }
+		auto start_idx = vertex_idx(start);
+		auto end_idx = vertex_idx(end);
+		graph[start_idx][end_idx] = true;
+		graph[end_idx][start_idx] = true;
     }
 
     cout << "printing graph: \n";
-    for (const auto &[vertex, adj_edges] : graph) {
-        cout << vertex << ": ";
-        for (const auto &edge : adj_edges) {
-            cout << edge << " ";
-        }
-        cout << "\n";
-    }
+	print_graph();
 
     cout << "starting traversal at start:\n";
-    uint64_t distinct_paths = 0;
-    // for (const auto &edge : graph["start"]) {
-    //     map<string, int> visited;
-    //     while (bfs(graph, visited, edge, distinct_paths)) {
-    //         // do nothing
-    //     }
-    //     cout << "\n";
-    // }
-    // cout << "distinct_paths: " << distinct_paths << endl;
+    uint64_t paths = 0;
+	auto start = find(begin(vertices), vertices.end(), "start");
+	auto end = find(begin(vertices), vertices.end(), "end");
+	if (start == vertices.end() || end == vertices.end()) throw invalid_argument("a start/end cave was not provided.");
+	
+	auto start_idx = distance(begin(vertices), start);
+	visited[start_idx] = true; // set start as visited
+	dfs(start_idx, distance(begin(vertices), end), paths);
+
+    cout << "paths: " << paths << endl;
     return 0;
 }
 
